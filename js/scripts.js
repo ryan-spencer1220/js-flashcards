3ff12e8789f4f47d7daf1844155756cacb129ab1
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-01-13 07:35:59 -0800

    Create initial flashcard set

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".clickable1").click(function () {
+    $(".js-showing").toggle();
+  });
+  $(".clickable2").click(function () {
+    $(".operator-showing").toggle();
+  });
+  $(".clickable3").click(function () {
+    $(".variable-showing").toggle();
+  });
+  $(".clickable4").click(function () {
+    $(".function-showing").toggle();
+  });
+});
